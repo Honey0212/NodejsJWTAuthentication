19f9cba3bf7d7dbe53d2c853b4c79ba073be683e
honey chauhan <honeychauhanchauhan0212@gmail.com>
2018-06-26 23:40:49 +0530

    Updated databse details

--- DAO/UserDAO.js
+++ DAO/UserDAO.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
     connectionLimit: 100,
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'grabbd',
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12244770',
+    password: 'UnzuUxJ4lQ',
+    database: 'sql12244770',
     port: 3306,
     debug: true,
     multipleStatements: true
